ff6308b796833c949f78ff62dba988a317652e1e
Silmi <129129210+SilmiCamilia@users.noreply.github.com>
2025-11-12 20:48:16 +0700

    update

--- ArrayNilai23.java
+++ ArrayNilai23.java
@@ -4,12 +4,17 @@ public class ArrayNilai23 {
     public static void main(String[] args) {
         Scanner uwo = new Scanner(System.in);
         int[] nilaiAkhir = new int[10];
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < nilaiAkhir.length; i++) {
             System.out.println("Masukkan nilai akhir ke-"+i+" : ");
             nilaiAkhir[i]=uwo.nextInt();
         }
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Nilai akhir ke-"+i+" adalah = "+nilaiAkhir[i]);
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            if(nilaiAkhir[i]>=78){
+                System.out.println("Nilai akhir ke-"+i+" lulus!");
+            }
+            else if(nilaiAkhir[i]<78){
+                System.out.println("Nilai akhir ke-"+i+" Tidak Lulus!");
+            }
         }
     }
 }
