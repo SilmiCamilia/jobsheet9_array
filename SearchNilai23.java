5247eb3a17b35e7660fd762f5743fc61580fd019
Silmi <129129210+SilmiCamilia@users.noreply.github.com>
2025-11-12 21:27:22 +0700

    pushhhhh

--- SearchNilai23.java
+++ SearchNilai23.java
@@ -3,18 +3,27 @@ import java.util.Scanner;
 public class SearchNilai23 {
     public static void main(String[] args) {
         Scanner ingput = new Scanner(System.in);
-        int[] nilai = {80, 85, 78, 96, 90, 82, 86};
-        int key = 90;
-        int hasil = 0;
+        System.out.print("Masukkan banyak nilai yg ingin diinput = ");
+        int z = ingput.nextInt();
+        int nilai[] = new int[z];
+        for (int i = 0; i < nilai.length; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+" =");
+            nilai[i]=ingput.nextInt();
+        }
+        System.out.print("Masukkan nilai yg dicari = ");
+        int key = ingput.nextInt();
+        boolean ditemukan = false;
         for (int i = 0; i < nilai.length; i++) {
             if (nilai[i] == key) {
-                hasil=i;
+                System.out.println("\nNilai " + key + " ketemu, merupakan nilai mahasiswa ke-" + (i + 1));
+                ditemukan = true;
                 break;
             }
         }
-        System.out.println();
-        System.out.println("Nilai "+key+" Ketemu di indeks ke-"+hasil);
-        System.out.println();
+
+        if (!ditemukan) {
+            System.out.println("\nNilai yang dicari tidak ditemukan.");
+        }
 
     }
 }
