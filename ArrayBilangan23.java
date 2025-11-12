3282dfce24e3c211195b4ca863249c34f1a11556
Silmi <129129210+SilmiCamilia@users.noreply.github.com>
2025-11-12 08:38:52 +0700

    coba2

--- ArrayBilangan23.java
+++ ArrayBilangan23.java
@@ -1,11 +1,12 @@
+
+
 public class ArrayBilangan23 {
     public static void main(String[] args) {
-        int [] bil = new int [4];
-        bil[0] = 5; bil[1] = 13; bil[2] = -7; bil[3] = 17;
-        System.out.println(bil[0]);        
-        System.out.println(bil[1]);
-        System.out.println(bil[2]);
-        System.out.println(bil[3]);
-        
+        double [] bil = new double [4];
+        bil[0] = 5.0; bil[1] = 12867; bil[2] = 7.5; bil[3] = 20000000;
+        for (int i = 0; i < 4; i++) {
+            System.out.println(bil[i]);
+        }
+
     }
 }
