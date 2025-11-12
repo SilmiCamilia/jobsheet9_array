9a250b12e5225d8c194c83fd7ed10f452ca2e08f
Silmi <129129210+SilmiCamilia@users.noreply.github.com>
2025-11-12 08:51:32 +0700

    PUSH

--- ArrayBilangan23.java
+++ ArrayBilangan23.java
@@ -2,8 +2,7 @@
 
 public class ArrayBilangan23 {
     public static void main(String[] args) {
-        double [] bil = new double [4];
-        bil[0] = 5.0; bil[1] = 12867; bil[2] = 7.5; bil[3] = 20000000;
+        double [] bil = {5.0,12867,7.5,20000000};
         for (int i = 0; i < 4; i++) {
             System.out.println(bil[i]);
         }
---
+++ ArrayNilai23.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ArrayNilai23 {
+    public static void main(String[] args) {
+        Scanner uwo = new Scanner(System.in);
+        int[] nilaiAkhir = new int[10];
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Masukkan nilai akhir ke-"+i+" : ");
+            nilaiAkhir[i]=uwo.nextInt();
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Nilai akhir ke-"+i+" adalah = "+nilaiAkhir[i]);
+        }
+    }
+}
